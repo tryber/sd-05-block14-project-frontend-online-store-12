f376cd3dc13ae5fb26e71757e31291556e60eb5d
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-24 18:52:05 -0300

    adicionando botao do carrinho de compras na pagina inicial e criando o componente ShopCart inicialmente vazio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+
 import * as api from './services/api'
 import './App.css';
 
+import ShopCart from './components/ShopCart';
+
 function App() {
   api.getCategories().then(categories => { console.log(categories) })
-  api.getProductsFromCategoryAndQuery('MLB1071', '').then(categorie => { console.log(categorie)})
+  api.getProductsFromCategoryAndQuery('MLB1071', '').then(categorie => { console.log(categorie) })
   return (
     <div className="App">
-    lalala
+      <button data-testid="shopping-cart-button">
+        <img src="https://img.icons8.com/ios/50/000000/add-shopping-cart.png" />
+      </button>
     </div>
   );
 }
