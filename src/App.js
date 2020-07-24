d9cfdeb2365b50754c7810a7362c8c940640272d
Virginia Duca <virginiambd.bio@gmail.com>
2020-07-24 16:44:39 -0300

    fetch-api-data-ready

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   api.getCategories().then(categories => { console.log(categories) })
+  api.getProductsFromCategoryAndQuery('MLB1071', '').then(categorie => { console.log(categorie)})
   return (
     <div className="App">
     lalala
