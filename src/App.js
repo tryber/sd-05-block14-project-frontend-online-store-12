af9596e998346215bb547c90e20d34f0de46d40a
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-24 20:44:25 -0300

    trocando tag button por link

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import * as api from './services/api'
 import './App.css';
 
 import ShopCart from './components/ShopCart';
 
+
+
 function App() {
   api.getCategories().then(categories => { console.log(categories) })
   api.getProductsFromCategoryAndQuery('MLB1071', '').then(categorie => { console.log(categorie) })
   return (
-    <div className="App">
-      <button data-testid="shopping-cart-button">
-        <img src="https://img.icons8.com/ios/50/000000/add-shopping-cart.png" />
-      </button>
-    </div>
+    <Router>
+      <div>
+        <div className="App">
+          <Link data-testid="shopping-cart-button" to="/cart">
+            <img
+              src="https://img.icons8.com/ios/50/000000/add-shopping-cart.png"
+              alt="Carrinho de Compras"
+            />
+          </Link>
+        </div>
+
+        <Switch>
+          <Route path="/cart" component={ShopCart} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
