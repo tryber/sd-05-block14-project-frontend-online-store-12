5c89beda19b8da491c16f33413171da9499c7ccb
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-27 16:04:25 -0300

    code climate

--- src/services/api.js
+++ src/services/api.js
@@ -6,10 +6,11 @@ export async function getCategories() {
 
 export async function getProductsFromCategoryAndQuery(CATEGORY_ID, QUERY) {
   if (CATEGORY_ID === '' && QUERY !== '') {
-    return await (await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`)).json();
+    return (await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`)).json();
   } else if (CATEGORY_ID !== '' && QUERY === '') {
-    return await (await fetch(` https://api.mercadolibre.com/sites/MLB/search?category=${CATEGORY_ID}`)).json();
+    return (await fetch(` https://api.mercadolibre.com/sites/MLB/search?category=${CATEGORY_ID}`)).json();
   } else if (CATEGORY_ID !== '' && QUERY !== '') {
-     return await (await fetch(` https://api.mercadolibre.com/sites/MLB/search?category=${CATEGORY_ID}&q=${QUERY}`)).json();
-  } console.log('ERRO, coloque parametros válidos');  
+    return (await fetch(` https://api.mercadolibre.com/sites/MLB/search?category=${CATEGORY_ID}&q=${QUERY}`)).json();
+  }
+  return console.log('ERRO, coloque parametros válidos');
 }
