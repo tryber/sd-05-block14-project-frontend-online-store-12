3b6b6ff899a5fa5e1aab418e5763b3cca68987be
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-27 19:59:18 -0300

    filtrando produtos por categoria e texto

--- src/components/CategoriesList.jsx
+++ src/components/CategoriesList.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import * as api from '../services/api';
 
 class CategoriesList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { categories: [] };
-  }
-
-  componentDidMount() {
-    api.getCategories().then((response) => this.setState({ categories: response }));
-  }
-
   render() {
-    const { categories } = this.state;
+    const { categorie: { name, id } } = this.props;
     return (
-      <div>
-        {categories.map((categorie) => (
-          <label data-testid="category" htmlFor={categorie.id}>
-            {categorie.name}
-            <input type="radio" id={categorie.id} name="produto" />
-          </label>
-        ))}
-      </div>
+      <label key={name} data-testid="category" htmlFor={id} className="produto">
+        {name}
+        <input type="radio" id={id} name="produto" />
+      </label>
     );
   }
 }
--- src/lista-produtos.js
+++ src/lista-produtos.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import CategoriesList from './components/CategoriesList';
+import './lista-produtos.css';
+import ProductList from './components/ProductList';
+import * as api from './services/api';
 
 class Lista extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { categories: [], products: [] };
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', (event) => {
+      if (event.target.id !== '') {
+        api.getProductsFromCategoryAndQuery(event.target.id, document.querySelector('.search-input').value)
+        .then((response) => this.setState({ products: response.results}));
+      }
+    })
+
+    api.getCategories().then((response) => this.setState({ categories: response }));
+  }
+
   render() {
+    const { categories, products } = this.state;
     return (
       <div className="lista-produtos">
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-        <input type="text" placeholder="" />
+        <div className="categorias">
+          {categories.map((categorie) => <CategoriesList key={categorie.name} categorie={categorie} />)}
+        </div>
+        <div>
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+          <input className="search-input" type="text" placeholder="" />
+          <ProductList products={products} />
+        </div>
+        <Link data-testid="shopping-cart-button" to="/cart">
+          <img
+            src="https://img.icons8.com/ios/50/000000/add-shopping-cart.png"
+            alt="Carrinho de Compras"
+          />
+        </Link>
       </div>
     );
   }
