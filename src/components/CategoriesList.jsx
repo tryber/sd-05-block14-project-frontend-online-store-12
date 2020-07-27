4b6f0294b4cdbd8e9b2fe84807e8601c2632f4c0
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-27 15:29:06 -0300

    code climate

--- src/components/CategoriesList.jsx
+++ src/components/CategoriesList.jsx
@@ -5,24 +5,19 @@ class CategoriesList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      categories: '',
+      categories: [],
     };
   }
 
   componentDidMount() {
-    api.getCategories().then((response) => this.setState({ categories: response }));
+    api.getCategories().then(response => this.setState({ categories: response }));
   }
 
   render() {
     const { categories } = this.state;
     return (
       <div>
-        {categories.map((categorie) => (
-          <label data-testid="category">
-            {categorie.name}
-            <input type="radio" id={categorie.id} name="produto" />
-          </label>
-        ))}
+        {categories.map((categorie) => <label data-testid="category">{categorie.name}<input type="radio" id={categorie.id} name="produto"/></label>)}
       </div>
     );
   }
