6c9dd8f1b6227d95ce2bc90238c86ca659f9e92e
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-07-27 14:52:21 -0300

    Card 2 - Caixa de texto de busca

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import * as api from './services/api'
 import './App.css';
+import Lista from './lista-produtos'
 
 function App() {
   api.getCategories().then(categories => { console.log(categories) })
   return (
     <div className="App">
     lalala
+    <Lista />
     </div>
   );
 }
---
+++ src/lista-produtos.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Lista extends Component {
+  render() { 
+    return (
+      <div className="lista-produtos">
+        <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+        <input type="text" placeholder="" />
+      </div>
+    );
+  }
+}
+ 
+export default Lista;
