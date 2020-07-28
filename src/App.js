2539cf958ff12e36ef7fcd9f2e506e1efd3d258f
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-07-28 17:13:45 -0300

    Form simplão - código quebrado

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Lista from './lista-produtos';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import ShopCart from './components/ShopCart';
-import CategoriesList from './components/CategoriesList';
 import ProductDetail from './components/ProductDetail';
+import Form from './form-avaliacao';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <CategoriesList />
-        <Lista />
-        <Link data-testid="shopping-cart-button" to="/cart">
-          <img
-            src="https://img.icons8.com/ios/50/000000/add-shopping-cart.png"
-            alt="Carrinho de Compras"
-          />
-        </Link>
-      </div>
-      <Switch>
-        <Route path="/cart" component={ShopCart} />
-        <Route path="/products/:id" component={ProductDetail} />
-      </Switch>
-    </Router>
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/cart" component={ShopCart} />
+          <Route path="/products/:id" component={ProductDetail} />
+          <Route path="/form" component={Form} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
