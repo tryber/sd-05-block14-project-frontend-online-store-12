24ea2ab2f1bfd15ea27f854a4e6db37ea659c065
Virginia Duca <virginiambd.bio@gmail.com>
2020-07-28 17:07:13 -0300

    alteracoes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Lista from './components/lista-produtos';
+import Lista from './lista-produtos';
 import './App.css';
 import ShopCart from './components/ShopCart';
 import ProductDetail from './components/ProductDetail';
 
-
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Lista} />
-        <Route path="/cart" component={ShopCart} />
-        <Route path="/products/:id" component={ProductDetail} />
-      </Switch>
-    </Router>
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Lista} />
+          <Route path="/cart" component={ShopCart} />
+          <Route path="/products/:id" component={ProductDetail} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
