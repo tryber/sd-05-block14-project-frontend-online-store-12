931092e5f1b6da6d41cc32bee2b65566782115cc
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-07-29 17:54:40 -0300

    Mudança de identação

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -11,27 +11,28 @@ class ProductDetail extends React.Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     const itemId = this.props.location.pathname.split('/');
-    api.getProductsFromCategoryAndQuery(id, '')
-      .then((response) => response.results.map((result) => {
+    api.getProductsFromCategoryAndQuery(id, '').then((response) =>
+      response.results.map((result) => {
         if (result.id === itemId[itemId.length - 1]) return this.setState({ product: result });
         return '';
-      }));
+      }),
+    );
   }
 
   render() {
     const { title, price, condition, thumbnail } = this.state.product;
     return (
       <div>
-      <div>
-        <h1 data-testid="produt-detail-name">{title}</h1>
-        <img src={thumbnail} alt="Product" />
-        <p>Valor: R${price}</p>
-        <p>Condição: {condition}</p>
-        <button type="button">Adicionar ao carrinho</button>
-      </div>
-      <section>
-        <Evaluation />
-      </section>
+        <div>
+          <h1 data-testid="produt-detail-name">{title}</h1>
+          <img src={thumbnail} alt="Product" />
+          <p>Valor: R${price}</p>
+          <p>Condição: {condition}</p>
+          <button type="button">Adicionar ao carrinho</button>
+        </div>
+        <section>
+          <Evaluation />
+        </section>
       </div>
     );
   }
