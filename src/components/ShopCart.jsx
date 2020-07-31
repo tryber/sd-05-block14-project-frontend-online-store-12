0788ddc4a0fa9a5a4e2e0b963a652b5edb2de8b5
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-07-30 21:26:23 -0300

    Acréscimo dos botões + e - no carrinho de compras

--- src/components/ShopCart.jsx
+++ src/components/ShopCart.jsx
@@ -4,8 +4,20 @@ import ProductListCart from './ProductListCart';
 class ShopCart extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { products: [] };
+    this.state = { products: [], items: 0 };
     this.carregarDados = this.carregarDados.bind(this);
+    this.addToCart = this.addToCart.bind(this);
+    this.removeProduct = this.removeProduct.bind(this);
+  }
+
+  addToCart(product) {
+    this.props.onClickAdd(product);
+    this.props.onClickIncrement();
+  }
+
+  removeProduct(product) {
+    this.props.onClickRemove(product);
+    this.props.onClickDecrement();
   }
 
   componentDidMount() {
@@ -25,6 +37,12 @@ class ShopCart extends React.Component {
     return (
       <div>
         <ProductListCart products={products} />
+        <div>
+        <button type="button" data-testid="product-increase-quantity" onClick={() => this.addToCart(products)}>+</button>
+            <p>{products.quantidade}</p>
+            <p data-testid="shopping-cart-product-quantity">{products.quantidade}</p>
+            <button type="button" data-testid="product-decrease-quantity" onClick={() => this.removeProduct(products)}>-</button>
+        </div>
       </div>
     );
   }
